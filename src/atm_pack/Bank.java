574a328f6d480b9a04769e987a169fd15bd66455
Vivek <tvivek966@gmail.com>
2025-06-27 17:59:06 +0530

    bank.java file

--- src/atm_pack/Bank.java
+++ src/atm_pack/Bank.java
@@ -1,5 +1,16 @@
 package atm_pack;
 
+import java.util.ArrayList;
+
 public class Bank {
+
+    private String name;
+
+    private ArrayList<User> user;
+
+    private ArrayList<Account> account;
+
+    
+
 
 }
